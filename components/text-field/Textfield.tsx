9369f007c1586668e8942bd900290e0656d6bdf4
qoqomi <energyy044@gmail.com>
2024-10-30 01:09:03 +0900

    feat: text field 추가

---
+++ components/text-field/Textfield.tsx
@@ -0,0 +1,31 @@
+import styled from "@emotion/native";
+import { forwardRef, ForwardRefRenderFunction } from "react";
+import { TextInput, TextInputProps } from "react-native";
+
+interface TextfieldProps extends TextInputProps {}
+export const _Textfield: ForwardRefRenderFunction<TextInput, TextfieldProps> = (
+  _props = {},
+  ref
+) => {
+  const { ...props } = _props;
+
+  return (
+    <Container>
+      <StyledInput ref={ref} {...props} allowFontScaling />
+    </Container>
+  );
+};
+
+export const TextField = forwardRef(_Textfield);
+const Container = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  flex-grow: 0;
+  padding: 4px 0px;
+  gap: 16px;
+`;
+const StyledInput = styled.TextInput`
+  flex: 1;
+`;
