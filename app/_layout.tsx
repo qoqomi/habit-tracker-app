dc618b9fa451d0cfa2d56a2ccb138f38150be95f
qoqomi <energyy044@gmail.com>
2024-10-31 00:57:26 +0900

    feat: react query 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,11 +6,13 @@ import "react-native-reanimated";
 import { MainStackNavigation } from "@/components/navigation/MainStackNavigation";
 import { DefaultBottomSheetProvider } from "@/context/bottom-sheet/DefaultBottomSheetProvider";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const queryClient = new QueryClient();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -26,12 +28,14 @@ export default function RootLayout() {
   }
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <BottomSheetModalProvider>
-        <DefaultBottomSheetProvider>
-          <MainStackNavigation />
-        </DefaultBottomSheetProvider>
-      </BottomSheetModalProvider>
-    </GestureHandlerRootView>
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <BottomSheetModalProvider>
+          <DefaultBottomSheetProvider>
+            <MainStackNavigation />
+          </DefaultBottomSheetProvider>
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
+    </QueryClientProvider>
   );
 }
