82a5e24dc23e281ae09f0d7e82b8698819c698a8
qoqomi <energyy044@gmail.com>
2024-11-01 13:30:03 +0900

    fix: 핸들러 로직 수정

--- components/bottom-sheet/share/FrequencyWheelPicker.tsx
+++ components/bottom-sheet/share/FrequencyWheelPicker.tsx
@@ -1,3 +1,7 @@
+import { Typography } from "@/components/typography/Typography";
+import { Frequency } from "@/features/home/apis/getHabit";
+import styled from "@emotion/native";
+import { useState } from "react";
 import { DefaultWheelPicker } from "../DefaultWheelPicker";
 
 const FREQUENCY = [
@@ -9,17 +13,47 @@ const FREQUENCY = [
 
 interface FrequencyWheelPickerProps {
   selectedValue: string;
-  onChange: (value: string) => void;
+  onClose: () => void;
+  onSubmit: (value: string) => void;
 }
 export const FrequencyWheelPicker = ({
   selectedValue,
-  onChange,
+  onClose,
+  onSubmit,
 }: FrequencyWheelPickerProps) => {
+  const [frequency, setFrequency] = useState<Frequency | string>(
+    selectedValue ?? "매일"
+  );
+  const handleChange = (value: string) => {
+    setFrequency(value);
+  };
+
   return (
-    <DefaultWheelPicker
-      selectedValue={selectedValue}
-      options={FREQUENCY}
-      onChange={onChange}
-    />
+    <>
+      <TopNavigation>
+        <SideButton onPress={onClose}>
+          <Typography variant="default">닫기</Typography>
+        </SideButton>
+        <SideButton onPress={() => onSubmit(frequency)}>
+          <Typography variant="default">완료</Typography>
+        </SideButton>
+      </TopNavigation>
+      <DefaultWheelPicker
+        selectedValue={selectedValue}
+        options={FREQUENCY}
+        onChange={handleChange}
+      />
+    </>
   );
 };
+const TopNavigation = styled.View`
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  padding: 0 16px;
+  z-index: 1;
+`;
+
+const SideButton = styled.Pressable`
+  width: 44px;
+`;
